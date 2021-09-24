0fb3585849d5400e7cf5c57a3b3e70ba24427341
Kemal Caner Arzuman <kemalcanerarzuman@gmail.com>
2021-09-24 11:05:50 +0300

    name and email hass been added to project

--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import "fmt"
 
 var (
 	helloWorld string
+	name       string
+	email      string
 )
 
 func main() {
 	helloWorld = "Hello, World"
 	fmt.Println(helloWorld)
+	name, email = "Kemal Caner Arzuman", "kemalcanerarzuman@gmail.com"
+	fmt.Printf("My name is %s, and my email is %s.", name, email)
 }
