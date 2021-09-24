43d64881e1082d294030b7cf8cb8ecb2b78d7b6d
Kemal Caner Arzuman <kemalcanerarzuman@gmail.com>
2021-09-24 11:12:46 +0300

    User struct ahs been created and Readme has been updated

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Demo Go Project
 
 This project was created to learn and exercise GO wit GIT.
+
+## Continuing 
--- main.go
+++ main.go
@@ -4,13 +4,17 @@ import "fmt"
 
 var (
 	helloWorld string
-	name       string
-	email      string
+	user       Person
 )
+
+type Person struct {
+	name, email string
+	age         int
+}
 
 func main() {
 	helloWorld = "Hello, World"
 	fmt.Println(helloWorld)
-	name, email = "Kemal Caner Arzuman", "kemalcanerarzuman@gmail.com"
-	fmt.Printf("My name is %s, and my email is %s.", name, email)
+	user.name, user.email, user.age = "Kemal Caner Arzuman", "kemalcanerarzuman@gmail.com", 29
+	fmt.Printf("User's name is %s, and email is %s, and age is %d.", user.name, user.email, user.age)
 }
